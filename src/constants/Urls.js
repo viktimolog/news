1bbdc88773aa7fd133c2d3420373fd36f7a15256
viktimolog <viktimolog@gmail.com>
2018-07-06 11:17:05 +0300

    added avatar from material-ui

--- src/constants/Urls.js
+++ src/constants/Urls.js
@@ -1,5 +1,5 @@
 const Urls = {
-  clients: 'https://raw.githubusercontent.com/viktimolog/clientslist/master/src/db/clients.json'
+  clients: 'https://raw.githubusercontent.com/viktimolog/news/master/src/stubs/clients.json'
 }
 
 export default Urls
